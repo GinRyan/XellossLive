7c3463982175aec3e889273d708ba470657251d6
GinRyan <lp8800lz@gmail.com>
2016-11-18 15:44:38 +0800

    AppBarLayout? bye~

--- app/src/main/java/org/xellossryan/xellosslive/MainActivity.java
+++ app/src/main/java/org/xellossryan/xellosslive/MainActivity.java
@@ -1,16 +1,24 @@
 package org.xellossryan.xellosslive;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private android.support.v7.widget.Toolbar toolbar;
+    private android.widget.RelativeLayout activitymain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"请通过图库或其他应用打开视频",Toast.LENGTH_SHORT).show();
-        finish();
+        this.activitymain = (RelativeLayout) findViewById(R.id.activity_main);
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("视频播放器");
+        Toast.makeText(this, "请通过图库或其他应用打开视频", Toast.LENGTH_SHORT).show();
     }
 }
